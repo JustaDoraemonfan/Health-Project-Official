a4ced673e477392f9539a386ced11e3a7cbfe2ee
JustaDoraemonfan <souvikkpatra06@gmail.com>
2025-09-23 01:10:47 +0530

    Issue resolved

--- client/src/hooks/useAppointments.js
+++ client/src/hooks/useAppointments.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import { appointmentAPI, authAPI, dashboardAPI } from "../services/api";
 
 export const useAppointments = () => {
