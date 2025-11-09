d1ca07af0ccfbee60cfef0e61f53468dc5ed1111
JustaDoraemonfan <souvikkpatra06@gmail.com>
2025-11-09 11:30:25 +0530

    This is a minor fix for deployment domain

--- client/src/utils/file.js
+++ client/src/utils/file.js
@@ -1,5 +1,5 @@
 // utils/file.js
 export const getPdfUrl = (filePath) => {
-  const baseURL = "http://localhost:5000"; // or your deployed API URL
+  const baseURL = process.env.CLIENT_URL || "http://localhost:5173"; // or your deployed API URL
   return `${baseURL}/${filePath.replace(/\\/g, "/")}`;
 };
