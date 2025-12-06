cea7a83ef55d95f9e873e64611db799172208dca
JustaDoraemonfan <souvikkpatra06@gmail.com>
2025-12-06 23:21:57 +0530

    appointment issue

--- client/src/patientConfig/BookConsultant/BookingSteps.jsx
+++ client/src/patientConfig/BookConsultant/BookingSteps.jsx
@@ -218,7 +218,7 @@ export const BookingSteps = ({
             {errors.appointmentTime && (
               <div className="flex items-center gap-2 mt-2 text-red-400 text-xs">
                 <AlertCircle className="w-4 h-4" />
-                {errors.appointmentTime}
+                {errors.}
               </div>
             )}
           </div>
